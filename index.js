eb21b4312713414ae3e2f69b79e99f8c04bad4b5
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2018-06-15 17:54:33 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,162 @@
+const http = require("http");
+const https = require("https");
+class HttpClient {
+  async GET(options, secure) {
+    return new Promise((fulfill, reject) => {
+      options = options || {};
+      options.headers = options.headers || {};
+      options.port = options.port || (secure ? 443 : 80);
+      options.method = "GET";
+      options.headers["User-Agent"] =
+        options.headers["User-Agent"] || "node.js";
+      options.headers["Content-Type"] =
+        options.headers["Content-Type"] || "application/json";
+      let client = secure ? https : http;
+      var req = client.request(options, function(res) {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on("data", function(d) {
+          body += d;
+        });
+        res.on("end", () => {
+          if (res.statusCode < 400)
+            fulfill({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+          else
+            reject({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+        });
+        res.on("error", err => {
+          reject(err);
+        });
+      });
+      req.end();
+    });
+  }
+  async POST(options, payload, secure) {
+    return new Promise((fulfill, reject) => {
+      options = options || {};
+      options.headers = options.headers || {};
+      options.port = options.port || (secure ? 443 : 80);
+      options.method = "POST";
+      options.headers["User-Agent"] =
+        options.headers["User-Agent"] || "node.js";
+      options.headers["Content-Type"] =
+        options.headers["Content-Type"] || "application/json";
+      options.headers["Content-Length"] = Buffer.byteLength(payload);
+      let client = secure ? https : http;
+      var req = client.request(options, function(res) {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on("data", function(d) {
+          body += d;
+        });
+        res.on("end", () => {
+          if (res.statusCode < 400)
+            fulfill({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+          else
+            reject({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+        });
+        res.on("error", err => {
+          reject(err);
+        });
+      });
+      req.write(payload);
+      req.end();
+    });
+  }
+  async PUT(options, payload, secure) {
+    return new Promise((fulfill, reject) => {
+      options = options || {};
+      options.headers = options.headers || {};
+      options.port = options.port || (secure ? 443 : 80);
+      options.method = "PUT";
+      options.headers["User-Agent"] =
+        options.headers["User-Agent"] || "node.js";
+      options.headers["Content-Type"] =
+        options.headers["Content-Type"] || "application/json";
+      options.headers["Content-Length"] = Buffer.byteLength(payload);
+      let client = secure ? https : http;
+      var req = client.request(options, function(res) {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on("data", function(d) {
+          body += d;
+        });
+        res.on("end", () => {
+          if (res.statusCode < 400)
+            fulfill({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+          else
+            reject({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+        });
+        res.on("error", err => {
+          reject(err);
+        });
+      });
+      req.write(payload);
+      req.end();
+    });
+  }
+  async DELETE(options, secure) {
+    return new Promise((fulfill, reject) => {
+      options = options || {};
+      options.headers = options.headers || {};
+      options.port = options.port || (secure ? 443 : 80);
+      options.method = "DELETE";
+      options.headers["User-Agent"] =
+        options.headers["User-Agent"] || "node.js";
+      options.headers["Content-Type"] =
+        options.headers["Content-Type"] || "application/json";
+      let client = secure ? https : http;
+      var req = client.request(options, function(res) {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on("data", function(d) {
+          body += d;
+        });
+        res.on("end", () => {
+          if (res.statusCode < 400)
+            fulfill({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+          else
+            reject({
+              status: res.statusCode,
+              headers: res.headers,
+              body: body
+            });
+        });
+        res.on("error", err => {
+          reject(err);
+        });
+      });
+      req.end();
+    });
+  }
+}
+const instance = new HttpClient();
+module.exports = instance;
